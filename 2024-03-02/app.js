d3d4fe31896fac66d1ca4aa7be91a182ab61df10
daniel-clubb <danclubb004@gmail.com>
2024-03-03 13:46:40 +0000

    added animation when clicking an option

---
+++ 2024-03-02/app.js
@@ -0,0 +1,10 @@
+const options = document.querySelectorAll(".option");
+
+options.forEach((option) => {
+    option.addEventListener("click", () => {
+        option.classList.add("option-clicked");
+        setTimeout(() => {
+            option.classList.remove("option-clicked");
+        }, 605);
+    });
+});
